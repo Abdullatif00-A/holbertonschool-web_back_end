961de149ae67093fc39892c21478ef76dfffb804
Abdullatif00-A <A.alzaher0@hotmail.com>
2025-08-13 08:55:49 +0000

    New

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python3
 """ Learning Pagination"""
-from typing from Tuple
 
 
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    In this method we will calculate the start index and end index
+    to reach a specific page with a limited page size.
 
-def index_range(page: int, page_size: int )
-      """ Simple helper function """
+    Args:
+        page (int): the page number we want to reach (starting from 1).
+        page_size (int): the size of each page.
 
+    Returns:
+        tuple: combination of the start_index and the end_index.
 
-      start = (page - 1) * page_size
-      end = pahe * pahe_size
-
-
-      return (start, end)
+    Formula:
+        n: (number of page)
+        p: (page size)
+        start_index = (n-1) * p
+        end_index = n * p
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
