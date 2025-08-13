72579c0c8fbdd9ece1436f0ba9187403a15af759
Abdullatif00-A <A.alzaher0@hotmail.com>
2025-08-13 08:50:55 +0000

    New

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Learning Pagination"""
+from typing from Tuple
+
+
+
+def index_range(page: int, page_size: int )
+      """ Simple helper function """
+
+
+      start = (page - 1) * page_size
+      end = pahe * pahe_size
+
+
+      return (start, end)
